34ccc51b59fcfd470e1042aadff15bb8a48ecc0e
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2022-10-15 09:56:57 -0400

    automation of invoice weeks by changing val in conn details

--- jobs/invoice_classes.py
+++ jobs/invoice_classes.py
@@ -1,8 +1,40 @@
+import os 
+import pandas as pd  
 from sqlalchemy.orm import declarative_base, sessionmaker
-from invoice_connection_details import THIS_WEEK
+from datetime import datetime
+from invoice_connection_details import OCT22_inv_wk_strt_days
+from typing import Optional
+from invoice_connection_details import JOB_CONTEXT, THIS_WEEK_NUM
 
-BASE = declarative_base()
-TABLE_NAME = f"alexander_patrie_prek_invoice_for_{THIS_WEEK}"
+
+class InvoiceWeek():
+    def __init__(self, 
+                 invoice_week:Optional[int]=THIS_WEEK_NUM, 
+                 inv_week_strt_days:Optional[list]=OCT22_inv_wk_strt_days):
+        self.invoice_week = invoice_week
+        self.CURRENT_YEAR = str(datetime.now().year)
+        self.CURRENT_MONTH = str(datetime.now().month)#<-IF ACCESSING OLD INVOICES, MANUALLY CHANGE THIS VALUE, ELSE AUTO MONTH!
+        self.inv_week_start_days = inv_week_strt_days
+        self.invoice_context = self.__getitem__(invoice_week)
+    
+    def __getitem__(self, week_num):
+        return self._generate_invoice_context(week_num, self.inv_week_start_days)
+        
+    def _generate_invoice_context(self, week_num:int, start_days:Optional[list]=OCT22_inv_wk_strt_days):
+        def get_week(month, start_days:list, week_num:int, year):
+            return f"week_of_{month}_{start_days[week_num]}_{year}" 
+        return get_week(self.CURRENT_MONTH, start_days, week_num, self.CURRENT_YEAR)
+    
+    def _generate_table_context(self, place_of_business:str):
+        this_week = self.invoice_context
+        table_name = f"alexander_patrie_{place_of_business}_invoice_for_{this_week}"
+        return this_week, table_name 
+    
+#####################################END_WEEK_NUM_CLASS#####################################
+
+iw = InvoiceWeek()
+THIS_WEEK, TABLE_NAME = iw._generate_table_context(JOB_CONTEXT)
+BASE=declarative_base()
 
 class Invoice(BASE):
     from sqlalchemy import Column, String, Integer
@@ -11,27 +43,66 @@ class Invoice(BASE):
     SERVICE_DATE = Column(String())
     SERVICE_RENDERED = Column(String())
     DURATION = Column(Integer())
+    
 #################################END_INVOICE_CLASS###############################
 
 
-from typing import Optional 
-import os 
-import pandas as pd    
-
-class Pencil(Invoice):
-    
+class Pencil(Invoice, InvoiceWeek):
     from sqlalchemy import create_engine
-    def __init__(self, engine:create_engine, connection:str, table_name:Optional[str]=TABLE_NAME,
-                 base:Optional[declarative_base]=BASE, new_invoice:Optional[bool]=True):
-        '''MUST SPECIFY TABLE_NAME!'''
+    import datetime
+    __current_year = str(datetime.datetime.now().year)
+    
+    def __init__(self, 
+                 engine:create_engine, 
+                 connection:str,  
+                 base:Optional[declarative_base]=BASE, 
+                 this_week:Optional[str]=THIS_WEEK,
+                 year:Optional[str]=__current_year,
+                 new_invoice:Optional[bool]=True):
+        '''ONE MUST specify the connection and engine when instantiating this class.
+           Think of the connection as the gas that powers the engine. Engine and 
+           connection work together to create a session and thus provide a landscape
+           by which the cursor(which is this class) can drill into and traverse the 
+           postgrSQL server and database(s)ß'''
+        super().__init__()
         session = sessionmaker(engine)
         self.local_session = session(bind=engine)
         self.connection = connection 
-        self.table_name = table_name
-        if new_invoice:
+        self.year = year 
+        self.this_week = this_week
+        self.project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        self.invoice_data_dir = self.__getitem__()[0]
+        if new_invoice is not False:
            base.metadata.create_all(engine)
+        if not os.path.exists(self.invoice_data_dir):
+            os.mkdir(self.invoice_data_dir)
         '''add dedicated dir for pdf, excel, and txt file!!'''
-    
+        
+        
+    def __getitem__(self):#, file_names:list):
+        def _get_dir_path() -> str:
+            _INVOICE_DIR_NAME = f"invoice_data_NMS_PREK_{self.year}"
+            return os.path.join(self.project_dir, _INVOICE_DIR_NAME)
+        
+        def _get_dirFiles_of_type(file_type:str) -> list:
+            '''get certian files in the data dir'''
+            file_list = []
+            for path, _, files in os.walk(_get_dir_path()):
+                for f in files:
+                    if f[-3:] == file_type:
+                        file_path = os.path.join(path, f)
+                        file_list.append(file_path)
+            return file_list
+        
+        def _get_data_file_path(file_name:str):
+            return os.path.join(_get_dir_path(), file_name) 
+        
+        data_dir_path = _get_dir_path()
+        excel_invoice_path = _get_data_file_path(f"{self.this_week}.xls")
+        pdf_invoice_path = _get_data_file_path(f"{self.this_week}.pdf")
+        return data_dir_path, excel_invoice_path, pdf_invoice_path
+        
+        
     def prompt_batch_insert(self) -> list:         
         '''method that prompts user to add info to batch_list'''   
         batch = []
@@ -47,6 +118,7 @@ class Pencil(Invoice):
                 batch.append(new_invoice_info)
         return batch
     
+    
     def insert_batch(self, batch_info):
         '''insert n_dim batch into the db'''
         for row in batch_info:
@@ -58,41 +130,98 @@ class Pencil(Invoice):
             self.local_session.commit()
             print('\n***new invoice added!!!***\n')
         
+        
     def prompt_create_invoice(self, write_batch:Optional[bool]=False):
-        '''will prompt the user for the amount of entries that they want to enter into
-           the db'''
+        '''will prompt the user for the amount of entries that they 
+           want to enter into the db'''
         batch = self.prompt_batch_insert()
         if write_batch:
             self.write_invoice_info_file(batch)
         return self.insert_batch(batch)
+            
                 
     def refresh_output_files(self, txt_file_path, excel_file_path):
         if os.path.exists(txt_file_path) and os.path.exists(excel_file_path):
             os.remove(txt_file_path)
             os.remove(excel_file_path)
             
+            
     def read_df_from_pg(self, sql) -> pd.DataFrame:
         return pd.read_sql(sql, self.connection)
+    
     
     def create_excel_from_df(self, excel_path:str, df:pd.DataFrame):
         df.to_excel(excel_path,  
                     header=True)
     
+    
     def _add_total_duration(self, df:pd.DataFrame):
         df["TOTAL_WEEK_DURATION"] = df["DURATION"].sum()
         df.iloc[1:4, 4] = ""
         return df 
+        
+        
+    from invoice_connection_details import _REC_PHONE
+    def send_text(self, df:pd.DataFrame, 
+                  txt_file_name:Optional[str]=f"{THIS_WEEK}_invoice_data.txt",
+                  rec_phone:Optional[str]=_REC_PHONE):
+        '''here the invoices will be sent via text message and saved as a text file
+           alongside the excel and pdf copy'''
+        from invoice_connection_details import _TM_TOKEN, _TM_UNAME
+        from textmagic.rest import TextmagicRestClient
+        username = _TM_UNAME
+        token = _TM_TOKEN
+        client = TextmagicRestClient(username, token)
+        
+        def extract_str(df):
+            data_str = " "
+            data_list = [x.tolist() for x in df.values]
+            for list in data_list:
+                for item in list:
+                    item = os.path.join(str(item), ", ")
+                    data_str = data_str + str(item)
+            return data_str.replace("/", "")
+        
+        def df_to_map_str(df):
+            keys = df['SERVICE_ID'].values.tolist()
+            values = df.drop(['SERVICE_ID'], axis=1).values.tolist()
+            map0 = dict(zip(keys, values))
+            map1 = str(map0)
+            
+            def save_txt(map_string):
+                with open(os.path.join(self.invoice_data_dir, txt_file_name), "w") as f:
+                    f.write(map_string)
+                    
+            save_txt(map1)
+            return map1 
+        
+        data_map_str = df_to_map_str(df)
+        print(f"A TEXT FILE CALLED {txt_file_name} WAS CREATED IN {self.invoice_data_dir}")
+        return client.messages.create(phones=rec_phone, text=data_map_str)
+        
 ##############################END_PENCIL_CLASS####################################
 
 
 import numpy as np
 import matplotlib.pyplot as plt
-from invoice_connection_details import THIS_WEEK
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib.font_manager import FontProperties
 
 class DfToPDF():
-    '''inspired by towarddatascience.com'''
+    '''eventually, please customize this class with self.attributes such that you 
+       change the color and layout/format of the pdf on the fly based on the season!
+       if time=winter, make blue, if time=spring, make green, etc. Customizable!'''
+    
+    def __init__(self, 
+                 this_week:Optional[str]=THIS_WEEK):
+        '''this_week may be changed to reflect and access an archived invoice...
+           if accessing data from a time other than the current week, please specify the week
+            attribute up instantiation in the following format: 
+            week_of_MONTH_INVOICE_WEEK_START_DAY_YEAR'''
+        self.this_week = this_week
+    
+    
+    #inspired by towarddatascience.com
     def _draw_as_table(self, df, pagesize):
         alternating_colors = [['white'] * len(df.columns), ['peachpuff'] * len(df.columns)] * len(df)
         alternating_colors = alternating_colors[:len(df)]
@@ -100,7 +229,7 @@ class DfToPDF():
         ax.axis('tight')
         ax.axis('off')
         space = "\n" * 10
-        title = f"~Alexander Patrie~\n\nInvoice for the week of:\n{THIS_WEEK[8:].replace('_', '/')}"
+        title = f"~Alexander Patrie~\n\nInvoice for the week of:\n{self.this_week[8:].replace('_', '/')}"
         fig.suptitle(title, ha="center", fontsize=20, 
                      bbox={"pad":0.7, "boxstyle":"square", "facecolor":"peachpuff"}, y=0.70, 
                      fontweight="bold")
@@ -118,6 +247,7 @@ class DfToPDF():
         the_table.scale(1, 4)
         fig.subplots_adjust(top=0.8)
         return fig
+  
   
     def dataframe_to_pdf(self, df, out_filename, numpages=(1,1), pagesize=(20, 14)):
         with PdfPages(out_filename) as pdf:
@@ -137,4 +267,5 @@ class DfToPDF():
                     pdf.savefig(fig, bbox_inches='tight')
                     print("\nPDF of Invoice created!\n")
                     plt.close()
+                    
 ####################################END_PDF_CLASS####################################
--- jobs/invoice_maker.py
+++ jobs/invoice_maker.py
@@ -1,61 +1,99 @@
-from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy import Column, String, Integer, create_engine, ForeignKey 
-from datetime import datetime
-import invoice_connection_details as icd 
-import os 
-from invoice_classes import Pencil, DfToPDF
-from pandas import DataFrame as pdDF, read_sql 
+import os
+from typing import Optional
+from pandas import DataFrame as pdDF 
+from sqlalchemy import create_engine
+from invoice_connection_details import WK0_BATCH, WK1_BATCH, WK2_BATCH, WK3_BATCH, WK4_BATCH
+from invoice_classes import Pencil, DfToPDF, InvoiceWeek, THIS_WEEK
 
+'''to change weeks, simply change the value of THIS_WEEK_NUM in invoice_connection_details!'''
 
-'''JOB ACTIONS'''
-def insert_invoice_batch(pencil:Pencil, batch_info:list):
-    return pencil.insert_batch(batch_info)
-
-def fetch_df(pencil:Pencil):
-    df0 = pencil.read_df_from_pg(f"SELECT * FROM alexander_patrie_prek_invoice_for_{icd.THIS_WEEK}")
-    df1 = pencil._add_total_duration(df0)
-    return df1
-    
-def create_excel_from_df(pencil:Pencil, excel_path:str, df:pdDF):
-    return pencil.create_excel_from_df(excel_path, df)
-
-def create_pdf_from_df(pdf:DfToPDF, df:pdDF, out_filename:str):
-    return pdf.dataframe_to_pdf(df, out_filename)
-
-
-'''MAIN ACTIVATION FUNCTION'''
-def main(new_invoice_batch:list):
+#JOB ACTIONS
+def PREPARE_CLASSES(make_new:Optional[bool]=True):
+    '''generate a tuple of outputs of len 3 (2 classes and 1 conn str)'''
+    from invoice_connection_details import conn_type, username, pwd, hostname, port_id, db_name
     
     def _define_pg_connection(dialect_and_driver, username, password, host, port, db_name) -> str:
         connection_string = f"{dialect_and_driver}://{username}:{password}@{host}:{port}/{db_name}"
         return connection_string
     
-    from invoice_connection_details import conn_type, username, pwd, hostname, port_id, db_name, THIS_WEEK
-    CONNECTION = _define_pg_connection(conn_type, username, pwd, hostname, port_id, db_name)
-    ENGINE = create_engine(CONNECTION, echo=True)
-    PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    INVOICE_DIR_PATH = f"{PROJECT_DIR}/invoice_data_PREK_2022"
-    if not os.path.exists(INVOICE_DIR_PATH):
-        os.mkdir(INVOICE_DIR_PATH)
-    INVOICE_EXCEL_PATH = f"{INVOICE_DIR_PATH}/{THIS_WEEK}.xls"
-    INVOICE_PDF_PATH = f"{INVOICE_DIR_PATH}/invoice_{THIS_WEEK}.pdf"
+    def generate_pencil(eng, make_new:bool):
+        return Pencil(engine=eng, 
+                      connection=_define_pg_connection(conn_type, username, pwd, 
+                                                       hostname, port_id, db_name), 
+                      new_invoice=make_new)
+
+    connection = _define_pg_connection(conn_type, username, pwd, hostname, port_id, db_name)
+    engine = create_engine(connection, echo=True)
+    pencil = generate_pencil(engine, make_new)
+    pdf = DfToPDF()
+    return pencil, pdf
+
+#global essentials
+MAKE_NEW = True
+PENCIL, PDF = PREPARE_CLASSES(MAKE_NEW)
+WEEK = InvoiceWeek()
+thisWEEK = WEEK.invoice_context
+
+#job actions based on defined global essentials
+def insert_invoice_batch(batch_info:list):
+    return PENCIL.insert_batch(batch_info)
+
+def fetch_df(this_week:Optional[str]=thisWEEK):
+    df0 = PENCIL.read_df_from_pg(f"SELECT * FROM alexander_patrie_prek_invoice_for_{this_week}")
+    df1 = PENCIL._add_total_duration(df0)
+    return df1
     
-    PEN = Pencil(engine=ENGINE,  
-                 connection=CONNECTION)
-    PDF = DfToPDF()
-    
-    insert_invoice_batch(pencil=PEN, batch_info=new_invoice_batch)
-    df0 = fetch_df(PEN)
-    create_excel_from_df(PEN, INVOICE_EXCEL_PATH, df0)
-    create_pdf_from_df(PDF, df0, INVOICE_PDF_PATH)
+def create_excel_from_df(excel_path:str, df:pdDF):
+    return PENCIL.create_excel_from_df(excel_path, df)
+
+def create_pdf_from_df(df:pdDF, out_filename:str):
+    return PDF.dataframe_to_pdf(df, out_filename)
+
+def send_invoice_text(df:pdDF):
+    return PENCIL.send_text(df)
+
+
+#main job action-activation function
+def main(new_invoice_batch:(list), week_number:int):
+    from os import path as p, mkdir
+    project_dir = PENCIL.project_dir
+    excel_invoice_path, pdf_invoice_path = PENCIL.__getitem__()[1], PENCIL.__getitem__()[2]
+    insert_invoice_batch(batch_info=new_invoice_batch[week_number])
+    df0 = fetch_df(thisWEEK)
+    create_pdf_from_df(df0, pdf_invoice_path)
+    create_excel_from_df(excel_invoice_path, df0)
 
 
 if __name__ == "__main__":
-    WK0_BATCH = [[0, "10/03/2022", "Music Class - PREK", 30],
-                 [1, "10/03/2022", "Music Class - TODDLER", 30],
-                 [2, "10/06/2022", "Music Class - PREK", 30],
-                 [3, "10/06/2022", "Music Class - TODDLER", 30]]
-    main(WK0_BATCH)
+    '''just run the weeks that you have DONE so far here as args in main()'''
+    main(new_invoice_batch=(WK0_BATCH, WK1_BATCH,
+                            WK2_BATCH, WK3_BATCH),
+        week_number=WEEK.invoice_week)
+    print("Thank you for using me. Job done.")
+
+
+
+
+
+'''
+#MAIN ACTIVATION FUNCTION
+def main(new_invoice_batch:(list), week_number:int):
+    from os import path as p, mkdir
+    project_dir = p.dirname(p.dirname(p.abspath(__file__)))
+    excel_invoice_path, pdf_invoice_path = PENCIL.__getitem__()[1], PENCIL.__getitem__()[2]
+    insert_invoice_batch(pencil=PENCIL, batch_info=new_invoice_batch[week_number])
+    df0 = fetch_df(PENCIL)
+    create_pdf_from_df(PDF, df0, pdf_invoice_path)
+    create_excel_from_df(PENCIL, excel_invoice_path, df0)
+
+    
+if __name__ == "__main__":
+    #just run the weeks that you have DONE so far here as args in main()
+    main(new_invoice_batch=(WK0_BATCH, WK1_BATCH,
+                            WK2_BATCH, WK3_BATCH),
+        week_number=PENCIL.invoice_week)
+    print("Thank you for using me. Job done.")
+'''
     
     
     
